31fde091422385296e78631b077a8aeb87369c36
Bredy <nov.ondrej@gmail.com>
2019-11-18 23:31:48 +0100

    fix compile warning

--- src/simpleServer/linux/linuxService.cpp
+++ src/simpleServer/linux/linuxService.cpp
@@ -491,7 +491,9 @@ void LinuxService::cleanWaitings() {
 
 static std::pair<FD,FD> makePipe() {
 	int fd[2];
-	pipe2(fd,O_CLOEXEC);
+	if (!pipe2(fd,O_CLOEXEC)) {
+		throw SystemException(errno, "Can't create pipe");
+	}
 	return {
 		FD(fd[0]),FD(fd[1])
 	};
